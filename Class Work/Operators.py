6e80cd95b2812e0f76fd30450f9c168bae1fbda5
siva <venkatasivakumarlachi1@gmail.com>
2025-11-12 10:27:37 +0530

    operators

---
+++ Class Work/Operators.py
@@ -0,0 +1,49 @@
+'''
+Python Operators
+'''
+#1.Arithmetic operators
+a=20
+b=10
+print("a+b:",a+b)#a+b: 30
+print("a-b:",a-b)#a-b: 10
+print("a*b:",a*b)#a*b: 200
+print("a/b:",a/b)#a/b: 2.0
+print("a//b:",a//b)#a//b: 2
+print("a%b:",a%b)#a%b: 0
+print("a*b:",a*b)#a*b: 102400000000
+#2.Comparsion Operators
+c=30
+d=40
+print('c<d:',c<d)#c<d: True
+print('c>d:',c<d)#c>d: False
+
+
+
+print('c<=d:',c<d)#c<=d: True
+print('c>=d:',c<d)#c>=d: False
+print('c!=d:',c<d)#c!=d: True
+print('c==d:',c<d)#c==d: False
+#3.Assignment Operators
+'''
+var = var op val
+var op=val
+'''
+e=100
+e+=100
+print('e+=100:',e)
+e-=100
+print('e-=100:',e)
+e*=100
+print('e*=100:',e)
+e/=100
+print('e/=100:',e)
+e//=100
+print('e//=100:',e)
+e%=100
+print('e%=100:',e)
+e**=100
+print('e**=100:',e)
+#output:
+#e+=100: 200
+#e-=100: 100
+#e*=100: 100…
